6403384a14492dbbbf7bf333ebc9587182432aec
JebaKishore18 <nadarkishore2@gmail.com>
2026-04-09 15:08:39 +0530

    Update Love.java

--- Love.java
+++ Love.java
@@ -1,5 +1,6 @@
 public class Love {
     public static void main(String[] args) {
         System.out.println("Love your Enemies! And Yourself, Just Like our heavenly Father Loved us ");
+        System.out.println("Love Everyone");
     }
 }
